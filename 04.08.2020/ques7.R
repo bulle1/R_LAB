1104fa4f19c3cbfde117ef206012bffdf13d6e2c
[Gautam] <[mailforvijay3@gmail.com]>
2020-08-23 21:49:48 +0530

    ques7 edited

--- 04.08.2020/ques7.R
+++ 04.08.2020/ques7.R
@@ -1,7 +1,9 @@
+
+
 n1 <- as.integer(readline("enter 1st n0"))
 n2 <- as.integer(readline("enter 2st n0"))
 n3 <- as.integer(readline("enter 3st n0"))
 n4 <- as.integer(readline("enter 4st n0"))
 l <- c(n1,n2,n3,n4)
 sort(l)
-cat("3rd larges no is",l[3])
+cat("3rd larges no is",l[2])
